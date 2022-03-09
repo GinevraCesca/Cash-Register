e1477f976bb15a3557354f688afb286f89bbf011
cescgine645 <cescgine645@SDS-L-H85HC63>
2022-03-09 14:21:41 -0500

    Add project files.

---
+++ Cash Register/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Cash_Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paybutton = new System.Windows.Forms.Button();
+            this.milkchocolate = new System.Windows.Forms.Label();
+            this.milkinput = new System.Windows.Forms.TextBox();
+            this.outputlable = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.whiteinput = new System.Windows.Forms.TextBox();
+            this.darkinput = new System.Windows.Forms.TextBox();
+            this.whitechocolate = new System.Windows.Forms.Label();
+            this.darkchocolate = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calcolatebutton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.subtotaloutput = new System.Windows.Forms.Label();
+            this.taxoutput = new System.Windows.Forms.Label();
+            this.totaloutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // paybutton
+            // 
+            this.paybutton.Location = new System.Drawing.Point(89, 306);
+            this.paybutton.Name = "paybutton";
+            this.paybutton.Size = new System.Drawing.Size(94, 36);
+            this.paybutton.TabIndex = 0;
+            this.paybutton.Text = "Pay";
+            this.paybutton.UseVisualStyleBackColor = true;
+            this.paybutton.Click += new System.EventHandler(this.paybutton_Click);
+            // 
+            // milkchocolate
+            // 
+            this.milkchocolate.AutoSize = true;
+            this.milkchocolate.Location = new System.Drawing.Point(39, 59);
+            this.milkchocolate.Name = "milkchocolate";
+            this.milkchocolate.Size = new System.Drawing.Size(77, 13);
+            this.milkchocolate.TabIndex = 1;
+            this.milkchocolate.Text = "Milk Chocolate";
+            // 
+            // milkinput
+            // 
+            this.milkinput.Location = new System.Drawing.Point(131, 56);
+            this.milkinput.Name = "milkinput";
+            this.milkinput.Size = new System.Drawing.Size(39, 20);
+            this.milkinput.TabIndex = 2;
+            // 
+            // outputlable
+            // 
+            this.outputlable.AutoSize = true;
+            this.outputlable.Location = new System.Drawing.Point(331, 56);
+            this.outputlable.Name = "outputlable";
+            this.outputlable.Size = new System.Drawing.Size(0, 13);
+            this.outputlable.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Subtotal";
+            // 
+            // whiteinput
+            // 
+            this.whiteinput.Location = new System.Drawing.Point(131, 82);
+            this.whiteinput.Name = "whiteinput";
+            this.whiteinput.Size = new System.Drawing.Size(39, 20);
+            this.whiteinput.TabIndex = 5;
+            // 
+            // darkinput
+            // 
+            this.darkinput.Location = new System.Drawing.Point(131, 108);
+            this.darkinput.Name = "darkinput";
+            this.darkinput.Size = new System.Drawing.Size(39, 20);
+            this.darkinput.TabIndex = 6;
+            // 
+            // whitechocolate
+            // 
+            this.whitechocolate.AutoSize = true;
+            this.whitechocolate.Location = new System.Drawing.Point(39, 85);
+            this.whitechocolate.Name = "whitechocolate";
+            this.whitechocolate.Size = new System.Drawing.Size(86, 13);
+            this.whitechocolate.TabIndex = 7;
+            this.whitechocolate.Text = "White Chocolate";
+            // 
+            // darkchocolate
+            // 
+            this.darkchocolate.AutoSize = true;
+            this.darkchocolate.Location = new System.Drawing.Point(39, 111);
+            this.darkchocolate.Name = "darkchocolate";
+            this.darkchocolate.Size = new System.Drawing.Size(81, 13);
+            this.darkchocolate.TabIndex = 8;
+            this.darkchocolate.Text = "Dark Chocolate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(176, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "$ 32,60 x Kg";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(176, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "$ 32,60 x Kg";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(176, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "$ 32,60 x Kg";
+            // 
+            // calcolatebutton
+            // 
+            this.calcolatebutton.Location = new System.Drawing.Point(89, 151);
+            this.calcolatebutton.Name = "calcolatebutton";
+            this.calcolatebutton.Size = new System.Drawing.Size(94, 28);
+            this.calcolatebutton.TabIndex = 12;
+            this.calcolatebutton.Text = "Calculate";
+            this.calcolatebutton.UseVisualStyleBackColor = true;
+            this.calcolatebutton.Click += new System.EventHandler(this.calcolatebutton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(39, 229);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Tax Amount";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(39, 256);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Total Price";
+            // 
+            // subtotaloutput
+            // 
+            this.subtotaloutput.AutoSize = true;
+            this.subtotaloutput.Location = new System.Drawing.Point(137, 201);
+            this.subtotaloutput.Name = "subtotaloutput";
+            this.subtotaloutput.Size = new System.Drawing.Size(0, 13);
+            this.subtotaloutput.TabIndex = 15;
+            // 
+            // taxoutput
+            // 
+            this.taxoutput.AutoSize = true;
+            this.taxoutput.Location = new System.Drawing.Point(137, 229);
+            this.taxoutput.Name = "taxoutput";
+            this.taxoutput.Size = new System.Drawing.Size(0, 13);
+            this.taxoutput.TabIndex = 16;
+            // 
+            // totaloutput
+            // 
+            this.totaloutput.AutoSize = true;
+            this.totaloutput.Location = new System.Drawing.Point(137, 256);
+            this.totaloutput.Name = "totaloutput";
+            this.totaloutput.Size = new System.Drawing.Size(0, 13);
+            this.totaloutput.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.totaloutput);
+            this.Controls.Add(this.taxoutput);
+            this.Controls.Add(this.subtotaloutput);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.calcolatebutton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.darkchocolate);
+            this.Controls.Add(this.whitechocolate);
+            this.Controls.Add(this.darkinput);
+            this.Controls.Add(this.whiteinput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputlable);
+            this.Controls.Add(this.milkinput);
+            this.Controls.Add(this.milkchocolate);
+            this.Controls.Add(this.paybutton);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Cash Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button paybutton;
+        private System.Windows.Forms.Label milkchocolate;
+        private System.Windows.Forms.TextBox milkinput;
+        private System.Windows.Forms.Label outputlable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox whiteinput;
+        private System.Windows.Forms.TextBox darkinput;
+        private System.Windows.Forms.Label whitechocolate;
+        private System.Windows.Forms.Label darkchocolate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button calcolatebutton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label subtotaloutput;
+        private System.Windows.Forms.Label taxoutput;
+        private System.Windows.Forms.Label totaloutput;
+    }
+}
+
---
+++ Cash Register/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Cash_Register
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        double milkprice = 2.50;
+        double whiteprice = 2.50;
+        double darkprice = 2.50;
+        int milkpieces = 0;
+        int whitepieces = 0;
+        int darkpieces = 0;
+        double subtotal = 0;
+        double taxrate = 0.13;
+        double taxamount = 0;
+        double totalprice = 0;
+
+            public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcolatebutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                milkpieces = Convert.ToInt32(milkinput.Text);
+                whitepieces = Convert.ToInt32(whiteinput.Text);
+                darkpieces = Convert.ToInt32(darkinput.Text);
+
+                subtotal = milkprice * milkpieces + whiteprice * whitepieces + darkprice * darkpieces;
+                taxamount = subtotal * taxrate;
+                totalprice = subtotal + taxamount;
+
+                subtotaloutput.Text = subtotal.ToString("C");
+                taxoutput.Text = taxamount.ToString("C");
+                totaloutput.Text = totalprice.ToString("C");
+            }
+
+            catch
+            {
+                subtotaloutput.Text = "Error";
+                taxoutput.Text = "";
+                totaloutput.Text = "";
+            }
+        }
+
+        private void paybutton_Click(object sender, EventArgs e)
+        {
+            outputlable.Text = $"MY AMAZING CASH REGISTER\n\n";
+            outputlable.Refresh();
+            Thread.Sleep(500);
+
+            outputlable.Text += $"{milkpieces} Milk Chocolate @{milkprice.ToString("C")}\n";
+            outputlable.Refresh();
+            Thread.Sleep(500);
+
+            outputlable.Text += $"{whitepieces} White Chocolate @{whiteprice.ToString("C")}\n";
+            outputlable.Refresh();
+            Thread.Sleep(500);
+
+            outputlable.Text += $"{darkpieces} Dark Chocolate @{darkprice.ToString("C")}\n\n";
+            outputlable.Refresh();
+            Thread.Sleep(500);
+
+            outputlable.Text += $"Subtotal.....................................{subtotal.ToString("C")}\n";
+            outputlable.Refresh();
+            Thread.Sleep(500);
+
+            outputlable.Text += $"Tax Amount...............................{taxamount.ToString("C")}\n";
+            outputlable.Refresh();
+            Thread.Sleep(500);
+
+            outputlable.Text += $"Total Price.................................{totalprice.ToString("C")}\n\n";
+            outputlable.Refresh();
+            Thread.Sleep(500);
+        }
+    }
+}
